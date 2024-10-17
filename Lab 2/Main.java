0c53441a4b965903096a8fbc5a66c7108fb06186
Skat77 <159482597+Skat77@users.noreply.github.com>
2024-10-17 22:11:28 +0200

    Added Task 2

--- Lab 2/Main.java
+++ Lab 2/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Main {
@@ -22,9 +23,46 @@ public class Main {
         return answer;
     }
 
+    //Task 2
+    public static int[] ArrayMerge(int[] arr1, int[] arr2) {
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        int[] arr3 = new int[len1 + len2];
+        int ind1 = 0, ind2 = 0, ind3= 0;
+        while (ind1 < len1 && ind2 < len2) {
+            if (arr1[ind1] <= arr2[ind2]) {
+                arr3[ind3] = arr1[ind1];
+                ind1++;
+                ind3++;
+            }
+            else {
+                arr3[ind3] = arr2[ind2];
+                ind2++;
+                ind3++;
+            }
+        }
+        //Добавление оставшихся элементов начальных массивов
+        while (ind1 < len1) {
+            arr3[ind3] = arr1[ind1];
+            ind1++;
+            ind3++;
+        }
+        while (ind2 < len2) {
+            arr3[ind3] = arr2[ind2];
+            ind2++;
+            ind3++;
+        }
+        return arr3;
+    }
+
     public static void main(String[] args) {
         //Task 1
         String str = "Good afternoon";
         System.out.println("Task 1: " + UniqueSubstr(str));
+
+        //Task 2
+        int[] array1 = {2, 6, 10, 16, 25, 76, 107};
+        int[] array2 = {1, 7, 8, 43, 54, 77};
+        System.out.println("Task 2: " + Arrays.toString(ArrayMerge(array1, array2)));
     }
 }
